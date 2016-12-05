4d39194d0a76e70b2cbe64aa21b72c10d4c39405
Tim Maddux <timothymaddux@gmail.com>
2016-12-05 13:08:56 -0600

    Arb stopping place.

---
+++ Council Tracker.Tests/DAL/ScrapeTesting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using Council_Tracker.Models;
+using Council_Tracker.DAL;
+
+namespace Council_Tracker.Tests.DAL
+{
+    [TestClass]
+    public class ScrapeTesting
+    {
+        private CouncilMemberData data { get; set; }
+
+        [TestMethod]
+        public void CanSeedViceMayorData()
+        {
+            WebClient client = new WebClient();
+            //CouncilMember vicemayor = new CouncilMember();
+
+            string vicemayor = data.seedViceMayor();
+            Assert.IsNotNull(vicemayor);
+        }
+    }
+}
--- Council Tracker/DAL/CouncilMemberData.cs
+++ Council Tracker/DAL/CouncilMemberData.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
+using System.Text.RegularExpressions;
+using Council_Tracker.Models;
+using System.IO;
 
 namespace Council_Tracker.DAL
 {
     public class CouncilMemberData
     {
+        WebClient client = new WebClient();
+
+        public string seedViceMayor()
+        {
+            CouncilMember viceMayor = new CouncilMember();
+            string rawHtml = client.DownloadString($"http://www.nashville.gov/Metro-Council/Metro-Council-Members/Vice-Mayor.aspx");
+            string officeRegex = "<h1>[A-Za-z]*\s[A-Za-z]*\s?[A-Za-z]*\s?[A-Za-z]*\s?<\/h1>";
+            //return viceMayor;
+            return rawHtml;
+        }
     }
 }
