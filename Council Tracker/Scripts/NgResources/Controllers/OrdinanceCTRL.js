2184eb1358197f48f6cf9fcd83a4e9bcdd3d0a58
Tim Maddux <timothymaddux@gmail.com>
2016-12-28 09:58:24 -0600

    re-organizing

---
+++ Council Tracker/Scripts/NgResources/Controllers/OrdinanceCTRL.js
@@ -0,0 +1,20 @@
+﻿app.controller("OrdinanceCTRL", function ($scope, $http) {
+    $scope.test = () => {
+        console.log("hey cowpoke. you've got the LandingCTRL working.");
+    }
+    $scope.ords = [];
+    $scope.running = false;
+    $scope.getOrds = () => {
+        if (!$scope.running) {
+            $scope.running = true;
+            $http.get("/api/Ordinance")
+            .success(function (response) {
+                $scope.ords = response;
+            })
+            .error(function (error) {
+                console.log(error);
+            });
+        }
+        return $scope.ords;
+    };
+});
--- Council Tracker/Scripts/NgResources/app.js
+++ Council Tracker/Scripts/NgResources/app.js
@@ -32,36 +32,5 @@ app.config(function ($routeProvider) {
 
 });
 
-//Controllers
-app.controller("OrdinanceCTRL", function ($scope, $http) {
-    $scope.test = () => {
-        console.log("hey cowpoke. you've got the LandingCTRL working.");
-    }
-    $scope.ords = [];
-    $scope.running = false;
-    $scope.getOrds = () => {
-        if (!$scope.running) {
-            $scope.running = true;
-            $http.get("/api/Ordinance")
-            .success(function (response) {
-                $scope.ords = response;
-            })
-            .error(function (error) {
-                console.log(error);
-            });
-        }
-        return $scope.ords;
-    };
-});
 
-app.controller("ResolutionCTRL", function ($scope) {
-    $scope.test = () => {
-        console.log("hey, here's the ResolutionCTRL .");
-    }
-});
 
-app.controller("CouncilMemberCTRL", function ($scope) {
-    $scope.test = () => {
-        console.log("hey cowpoke, you've got the councilmemberCTRL workin'.");
-    }
-});
