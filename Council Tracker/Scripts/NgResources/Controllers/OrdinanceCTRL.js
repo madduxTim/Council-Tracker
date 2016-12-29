4a14101e39d7e0e8263a97ebd0c7ad81af97e43f
Tim Maddux <timothymaddux@gmail.com>
2016-12-28 18:01:57 -0600

    stopping place.

--- Council Tracker/Scripts/NgResources/Controllers/OrdinanceCTRL.js
+++ Council Tracker/Scripts/NgResources/Controllers/OrdinanceCTRL.js
@@ -18,17 +18,15 @@
         return $scope.ords;
     };
 
-    $scope.ord = {};
+    $scope.ord = [];
     $scope.getOrd = (ordNumber) => {
-        if (!$scope.running) {
-            $scope.running = true;
             $http.get("/api/Ordinance")
-                .success(function (response) {
-                    $scope.ord = response;
-                })
-                .error(function (error) {
-                    console.log(error);
-                });
-        };
-        return $scope.ord;
+            .success(function (response) {
+                $scope.ord = response;
+            })
+            .error(function (error) {
+                console.log(error);
+            });
+        return $scope.ords;
     };
+});
